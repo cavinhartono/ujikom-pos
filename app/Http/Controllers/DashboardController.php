c10c70f376c111d5d367ac91db902a891a0d55a3
Cavin Hartono Putra <muhammadcavin25@gmail.com>
2023-03-01 11:15:56 +0700

    master

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Categories;
 use App\Models\Customer;
+use App\Models\Order;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -12,11 +13,12 @@ class DashboardController extends Controller
     public function index()
     {
         $users = User::with('roles')->whereNotNull('last_seen')->orderBy('last_seen', "DESC")->paginate(5);
+        $orders = Order::all();
         $foods = Categories::where('name', 'LIKE', "makanan")->count();
         $drinks = Categories::where('name', 'LIKE', "minunan")->count();
         $others = Categories::where('name', 'NOT LIKE', "makanan")->where('name', 'NOT LIKE', "minunan")->count();
         $customers = Customer::all();
         $customerLast = Customer::orderBy('created_at', 'DESC')->paginate(5);
-        return view('dashboard.index', compact(['users', 'customers', 'customerLast', 'foods', 'drinks', 'others']));
+        return view('dashboard.index', compact(['users', 'customers', 'customerLast', 'foods', 'drinks', 'others', 'orders']));
     }
 }
