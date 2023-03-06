0fb091dd6260a283bd5e5a6e625903857c5af52b
Cavin Hartono Putra <muhammadcavin25@gmail.com>
2023-03-06 15:25:00 +0700

    master

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,6 +5,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="shortcut icon" href="{{ asset('assets/icons/cube-sharp.svg') }}" type="image/x-icon">
   <link rel="stylesheet" href="{{ asset('assets/css/styles.css') }}">
   <title>@stack('title')</title>
   @stack('css')
