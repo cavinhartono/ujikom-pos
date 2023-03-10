6de89944f7d6b079eb12109166b64c2d266b71dc
Cavin Hartono Putra <muhammadcavin25@gmail.com>
2023-03-10 11:13:17 +0700

    master

---
+++ app/Http/Controllers/CategoriesController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Categories;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class CategoriesController extends Controller
+{
+    public function index()
+    {
+        $categories = Categories::all();
+        return view('products.index', compact(['categories']));
+    }
+
+    public function create()
+    {
+        $users = User::with('roles')->whereNotNull('last_seen')->orderBy('last_seen', "DESC")->paginate(5);
+        return view('categories.create', compact(['users']));
+    }
+
+    public function store(Request $request)
+    {
+        Categories::create($request->all());
+        return redirect('/products')->with('success', `$request->name telah ditambahkan`);
+    }
+
+    public function edit($id)
+    {
+        $category = Categories::find($id);
+        return view('categories.edit', compact(['category']));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $category = Categories::find($id);
+        $category->update($request->all());
+
+        return redirect('/products')->with('success', `$request->name telah diedit`);
+    }
+
+    public function delete($id)
+    {
+        $category = Categories::find($id);
+        $category->delete();
+        return redirect('/products')->with('primary', 'Sudah dihapus!');
+    }
+}
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -114,6 +114,44 @@ Produk | Shopcube
     </tbody>
   </table>
 </div>
+<div class="categories">
+  <div class="label full-content between" style="align-items: center;">
+    <div class="label flex gap">
+      <a href="/categories/create" class="btn primary link">Tambah</a>
+    </div>
+    <div class="field" style="width: 300px;">
+      <div class="input">
+        <input type="text" class="input-form" id="search" placeholder="Pencarian">
+      </div>
+    </div>
+  </div>
+  <ul class="category flex gap">
+    @forelse($categories as $category)
+    <li class="list between gap">
+      <a href="/categories/edit/{{ $category->id }}" class="link">
+        <h2 class="subtitle">
+          {{ $category->name }}
+        </h2>
+        <div class="action">
+          <a href="/categories/delete/{{ $category->id }}" class="link">
+            <span class="icon">
+              <svg xmlns="http://www.w3.org/2000/svg" width="auto" fill="currentColor" height="auto" viewBox="0 0 512 512">
+                <title>Delete</title>
+                <polygon points="337.46 240 312 214.54 256 270.54 200 214.54 174.54 240 230.54 296 174.54 352 200 377.46 256 321.46 312 377.46 337.46 352 281.46 296 337.46 240" style="fill: none" />
+                <polygon points="337.46 240 312 214.54 256 270.54 200 214.54 174.54 240 230.54 296 174.54 352 200 377.46 256 321.46 312 377.46 337.46 352 281.46 296 337.46 240" style="fill: none" />
+                <path d="M64,160,93.74,442.51A24,24,0,0,0,117.61,464H394.39a24,24,0,0,0,23.87-21.49L448,160ZM312,377.46l-56-56-56,56L174.54,352l56-56-56-56L200,214.54l56,56,56-56L337.46,240l-56,56,56,56Z" />
+                <rect x="32" y="48" width="448" height="80" rx="12" ry="12" />
+              </svg>
+            </span>
+          </a>
+        </div>
+      </a>
+    </li>
+    @empty
+    <li class="list">Tidak ada</li>
+    @endforelse
+  </ul>
+</div>
 @endsection
 
 @push('js')
