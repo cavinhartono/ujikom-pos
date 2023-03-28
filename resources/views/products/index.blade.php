3b36fe9d8c65d26ee6177cb16d5c067661b2f5f0
Cavin Hartono Putra <muhammadcavin25@gmail.com>
2023-03-28 11:19:18 +0700

    master

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -127,7 +127,14 @@ Produk | Shopcube
 <div class="categories" style="position: relative; margin-top: var(--md)">
   <div class="label full-content between" style="align-items: center;">
     <div class="label flex gap">
-      <a href="/categories/create" class="btn primary link">Tambah</a>
+      <a href="/categories/create" class="btn primary link">
+        <span class="icon center">
+          <svg xmlns="http://www.w3.org/2000/svg" width="auto" height="auto" fill="currentColor" viewBox="0 0 512 512">
+            <line x1="256" y1="112" x2="256" y2="400" style="fill:none;stroke:#fff;stroke-linecap:square;stroke-linejoin:round;stroke-width:32px" />
+            <line x1="400" y1="256" x2="112" y2="256" style="fill:none;stroke:#fff;stroke-linecap:square;stroke-linejoin:round;stroke-width:32px" />
+          </svg>
+        </span>
+      </a>
     </div>
     <div class="field" style="width: 300px;">
       <div class="input">
