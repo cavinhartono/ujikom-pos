51cfcc76758238e3e2170b611dea4a2545bd9f1f
Cavin Hartono Putra <muhammadcavin25@gmail.com>
2023-04-01 11:15:44 +0700

    master

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -15,13 +15,6 @@ Transaksi | Shopcube
   <h2 class="subtitle">Saat ini</h2>
 </div>
 <div class="orders">
-  <div class="label full-content flex" style="justify-content: flex-end;">
-    <div class="field" style="width: 300px">
-      <div class="input">
-        <input type="text" class="input-form" id="search" placeholder="Pencarian">
-      </div>
-    </div>
-  </div>
   <table class="table" id="table" style="margin: var(--md) 0;">
     <thead>
       <tr>
@@ -60,54 +53,3 @@ Transaksi | Shopcube
   </table>
 </div>
 @endsection
-
-@push('js')
-<script src="{{ asset('assets/js/plugins/jquery.js') }}"></script>
-<script>
-  $(document).ready(function() {
-    $(document).on('keyup', '#search', function() {
-      const search = $(this).val();
-
-      $.ajaxSetup({
-        headers: {
-          'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-        }
-      });
-
-      $.ajax({
-        type: 'post',
-        url: `/transactions/search`,
-        data: {
-          search
-        },
-        dataType: 'json',
-        success: function(response) {
-          $('tbody').html("");
-          $.each(response, function(key, order) {
-            $('tbody').append(`
-              <tr>
-                <td> - </td>
-                <td>${order.created_at}</td>
-                <td>${order.customer.map(x => {return x.name})}</td>
-                <td>${order.order_item.map((x) => x.price)}</td>
-                <td>${order.order_item.map((x) => x.accept)}</td>
-                <td>
-                  <a href="/transaction/${order.id}/view" class="link btn secondary">
-                    <span class="icon">
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" width="auto" height="auto" viewBox="0 0 512 512">
-                        <title>View ${order.customer.map((x) => x.name)} </title>
-                        <circle cx="256" cy="256" r="64" />
-                        <path d="M394.82,141.18C351.1,111.2,304.31,96,255.76,96c-43.69,0-86.28,13-126.59,38.48C88.52,160.23,48.67,207,16,256c26.42,44,62.56,89.24,100.2,115.18C159.38,400.92,206.33,416,255.76,416c49,0,95.85-15.07,139.3-44.79C433.31,345,469.71,299.82,496,256,469.62,212.57,433.1,167.44,394.82,141.18ZM256,352a96,96,0,1,1,96-96A96.11,96.11,0,0,1,256,352Z" />
-                      </svg>
-                    </span>
-                  </a>
-                </td>
-              </tr>
-            `)
-          });
-        }
-      })
-    })
-  })
-</script>
-@endpush
